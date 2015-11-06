bc3dc4082be46ba6e66a7292bef430e2e5be6d45
Yurii <yusk90@users.noreply.github.com>
2015-11-06 13:34:48 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,44 @@
 'use strict';
 
 var gulp = require('gulp'),
+    gulpIf = require('gulp-if'),
     requirejsOptimize = require('gulp-requirejs-optimize');
+
+var commonIncludes = [
+    'common',
+    'jquery',
+    'underscore',
+    'backbone',
+    'localstorage',
+    'helpers',
+    'BaseView',
+    'UserModel',
+    'UsersCollection',
+    'users'
+];
+
+var commonOptions = {
+    mainConfigFile: 'js/common.js',
+    name: 'common',
+    include: commonIncludes
+};
+
+var moduleOptions = function(file) {
+    var moduleName = file.relative.split('-')[0];
+
+    return {
+        mainConfigFile: 'js/common.js',
+        name: moduleName + '-script',
+        include: 'app/' + moduleName + '-init',
+        exclude: commonIncludes
+    }
+};
 
 gulp.task('scripts', function() {
     return gulp.src(['js/*.js'])
-        .pipe(requirejsOptimize())
+        .pipe(gulpIf('common.js',
+            requirejsOptimize(commonOptions),
+            requirejsOptimize(moduleOptions)))
         .pipe(gulp.dest('dist/js'));
 });
 
